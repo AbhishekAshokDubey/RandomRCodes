4d647fd286ebfbc466ea4d3a8b2c85be2bcaabdb
Abhishek Dubey <AbhishekAshokDubey@users.noreply.github.com>
2016-06-21 20:23:37 +0530

    Update and rename plot.R to plot_n_save.R

--- plot_n_save.R
+++ plot_n_save.R
@@ -1,6 +1,8 @@
 library(ggplot2)
 library(gridExtra)
 
+jpeg('rplot.jpg')
+
 a = runif(10,1,5)
 b = runif(10,3,10)
 k = as.data.frame(cbind(a,b))
@@ -15,7 +17,7 @@ test_fit <- ggplot(k, aes(c(1:length(k$a)),a)) +
 
 grid.arrange(train_fit,test_fit ,ncol=1, nrow =2)
 
-
+dev.off()
 
 # check
 # http://www.sthda.com/english/wiki/ggplot2-easy-way-to-mix-multiple-graphs-on-the-same-page-r-software-and-data-visualization
